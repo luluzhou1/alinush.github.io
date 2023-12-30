194847474254589be2bc7c495d2b85918c268a15
Alin Tomescu <tomescu.alin@gmail.com>
2023-12-30 14:39:03 +0200

    upgrade to MathJax 3.2.2

--- grep.sh
+++ grep.sh
@@ -1,1 +1,5 @@
-grep --color=always --exclude-dir=vendor --exclude-dir=.jekyll-cache --exclude="*.md" --exclude-dir=_site --exclude=feed.xml -Irn $@
+grep --color=always --exclude-dir=vendor --exclude-dir=.jekyll-cache --exclude="*.md" --exclude-dir=_site --exclude-dir=.git --exclude=feed.xml -Irn $@
+
+echo
+echo "Note: Excludes *.md files and useless directories (e.g., .git, vendor, etc.)"
+echo
